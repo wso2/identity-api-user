0b19dac6f30194882e6834a714a8a7138edef049
isura <isura@wso2.com>
2019-08-06 19:45:04 +0530

    Generate java classes based on swagger updaetes

--- components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/association/v1/MeApi.java
+++ components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/association/v1/MeApi.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.identity.rest.api.user.association.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,15 +45,18 @@ public class MeApi  {
 
    @Autowired
    private MeApiService delegate;
+
     @DELETE
     @Path("/associations")
     @Consumes({ "application/json" })
     @Produces({ "application/json" })
     @io.swagger.annotations.ApiOperation(value = "Delete all my user associations", notes = "This API is used to delete all associations of the auhtentiated user.", response = void.class)
     @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 200, message = "OK"),
+        @io.swagger.annotations.ApiResponse(code = 204, message = "No content"),
         
         @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
+        
+        @io.swagger.annotations.ApiResponse(code = 403, message = "Resource Forbidden"),
         
         @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error") })
 
@@ -57,6 +76,8 @@ public class MeApi  {
         
         @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
         
+        @io.swagger.annotations.ApiResponse(code = 403, message = "Resource Forbidden"),
+        
         @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error") })
 
     public Response meAssociationsGet()
@@ -74,6 +95,8 @@ public class MeApi  {
         @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
         
         @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
+        
+        @io.swagger.annotations.ApiResponse(code = 403, message = "Resource Forbidden"),
         
         @io.swagger.annotations.ApiResponse(code = 409, message = "Conflict"),
         
@@ -95,7 +118,11 @@ public class MeApi  {
         
         @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
         
-        @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error") })
+        @io.swagger.annotations.ApiResponse(code = 403, message = "Resource Forbidden"),
+        
+        @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error"),
+        
+        @io.swagger.annotations.ApiResponse(code = 501, message = "Not Implemented") })
 
     public Response meAssociationsSwitchPut(@ApiParam(value = "" ,required=true ) AssociationSwitchRequestDTO switchUserReqeust)
     {
--- components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/association/v1/UserIdApi.java
+++ components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/association/v1/UserIdApi.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.identity.rest.api.user.association.v1;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,9 +51,11 @@ public class UserIdApi  {
     @Produces({ "application/json" })
     @io.swagger.annotations.ApiOperation(value = "Delete user's all user associations", notes = "This API is used to delete all associations of the  user.", response = void.class)
     @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 200, message = "OK"),
+        @io.swagger.annotations.ApiResponse(code = 204, message = "No content"),
         
         @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized request"),
+        
+        @io.swagger.annotations.ApiResponse(code = 403, message = "Resource Forbidden"),
         
         @io.swagger.annotations.ApiResponse(code = 404, message = "Not Found"),
         
@@ -59,6 +77,8 @@ public class UserIdApi  {
         
         @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized request"),
         
+        @io.swagger.annotations.ApiResponse(code = 403, message = "Resource Forbidden"),
+        
         @io.swagger.annotations.ApiResponse(code = 404, message = "Not Found"),
         
         @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error") })
@@ -78,6 +98,8 @@ public class UserIdApi  {
         @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
         
         @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
+        
+        @io.swagger.annotations.ApiResponse(code = 403, message = "Resource Forbidden"),
         
         @io.swagger.annotations.ApiResponse(code = 409, message = "Conflict"),
         
