2fd2341c6dbb421596dd96e87d1a56ac3d545136
tharindu-bandara <bandara14@live.com>
2019-11-08 04:18:31 +0530

    [Generated Files] Refactor existing and add federated association APIs

--- components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/association/v1/MeApiService.java
+++ components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/association/v1/MeApiService.java
@@ -22,7 +22,7 @@ import org.wso2.carbon.identity.rest.api.user.association.v1.dto.*;
 import org.wso2.carbon.identity.rest.api.user.association.v1.dto.ErrorDTO;
 import org.wso2.carbon.identity.rest.api.user.association.v1.dto.UserDTO;
 import org.wso2.carbon.identity.rest.api.user.association.v1.dto.AssociationUserRequestDTO;
-import org.wso2.carbon.identity.rest.api.user.association.v1.dto.AssociationSwitchRequestDTO;
+import org.wso2.carbon.identity.rest.api.user.association.v1.dto.FederatedAssociationDTO;
 
 import java.util.List;
 
@@ -32,9 +32,19 @@ import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import javax.ws.rs.core.Response;
 
 public abstract class MeApiService {
-    public abstract Response meAssociationsDelete();
-    public abstract Response meAssociationsGet();
-    public abstract Response meAssociationsPost(AssociationUserRequestDTO association);
-    public abstract Response meAssociationsSwitchPut(AssociationSwitchRequestDTO switchUserReqeust);
-}
 
+    public abstract Response meAssociationsAssociatedUserIdDelete(String associatedUserId);
+
+    public abstract Response meAssociationsDelete();
+
+    public abstract Response meAssociationsGet();
+
+    public abstract Response meAssociationsPost(AssociationUserRequestDTO association);
+
+    public abstract Response meFederatedAssociationsDelete();
+
+    public abstract Response meFederatedAssociationsGet();
+
+    public abstract Response meFederatedAssociationsIdDelete(String id);
+
+}
