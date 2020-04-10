0a98eeee6815507bc4eaed95ed025c691dcadbb1
avishkajayasundara <avishkaaj@live.com>
2020-04-10 13:00:07 +0530

    Added deviceId to Registrationrequest and DiscoveryData classes

--- components/org.wso2.carbon.identity.api.user.biometric.device/org.wso2.carbon.identity.api.user.biometric.device.handler.v1/src/gen/java/org/wso2/carbon/identity/api/user/biometric/device/handler/v1/model/DiscoveryDataDTO.java
+++ components/org.wso2.carbon.identity.api.user.biometric.device/org.wso2.carbon.identity.api.user.biometric.device.handler.v1/src/gen/java/org/wso2/carbon/identity/api/user/biometric/device/handler/v1/model/DiscoveryDataDTO.java
@@ -20,18 +20,18 @@
 package org.wso2.carbon.identity.api.user.biometric.device.handler.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.Valid;
 import java.util.Objects;
 import java.util.UUID;
-import javax.validation.Valid;
 
 /**
  * .
  **/
-public class DiscoveryDataDTO  {
-  
+public class DiscoveryDataDTO {
+
+    private String id;
     private String username;
     private String tennantDomain;
     private String userStoreDomain;
@@ -40,113 +40,144 @@ public class DiscoveryDataDTO  {
     private UUID challenge;
 
     /**
-    **/
+     *
+     **/
+    public DiscoveryDataDTO id(String id) {
+
+        this.id = id;
+        return this;
+    }
+
+    @ApiModelProperty(example = "000AA11", value = "")
+    @JsonProperty("username")
+    @Valid
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     **/
     public DiscoveryDataDTO username(String username) {
 
         this.username = username;
         return this;
     }
-    
+
     @ApiModelProperty(example = "admin", value = "")
     @JsonProperty("username")
     @Valid
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
     /**
-    **/
+     *
+     **/
     public DiscoveryDataDTO tennantDomain(String tennantDomain) {
 
         this.tennantDomain = tennantDomain;
         return this;
     }
-    
+
     @ApiModelProperty(example = "mainDomain", value = "")
     @JsonProperty("tennantDomain")
     @Valid
     public String getTennantDomain() {
         return tennantDomain;
     }
+
     public void setTennantDomain(String tennantDomain) {
         this.tennantDomain = tennantDomain;
     }
 
     /**
-    **/
+     *
+     **/
     public DiscoveryDataDTO userStoreDomain(String userStoreDomain) {
 
         this.userStoreDomain = userStoreDomain;
         return this;
     }
-    
+
     @ApiModelProperty(example = "secondary", value = "")
     @JsonProperty("userStoreDomain")
     @Valid
     public String getUserStoreDomain() {
         return userStoreDomain;
     }
+
     public void setUserStoreDomain(String userStoreDomain) {
         this.userStoreDomain = userStoreDomain;
     }
 
     /**
-    **/
+     *
+     **/
     public DiscoveryDataDTO registrationUrl(String registrationUrl) {
 
         this.registrationUrl = registrationUrl;
         return this;
     }
-    
+
     @ApiModelProperty(example = "localhost:9443/t/{tenant_context}/api/users/v1/me/device/", value = "")
     @JsonProperty("registrationUrl")
     @Valid
     public String getRegistrationUrl() {
         return registrationUrl;
     }
+
     public void setRegistrationUrl(String registrationUrl) {
         this.registrationUrl = registrationUrl;
     }
 
     /**
-    **/
+     *
+     **/
     public DiscoveryDataDTO authenticationUrl(String authenticationUrl) {
 
         this.authenticationUrl = authenticationUrl;
         return this;
     }
-    
+
     @ApiModelProperty(example = "localhost:9443/t/{tenant_context}/api/users/v1/me/device/biometric", value = "")
     @JsonProperty("authenticationUrl")
     @Valid
     public String getAuthenticationUrl() {
         return authenticationUrl;
     }
+
     public void setAuthenticationUrl(String authenticationUrl) {
         this.authenticationUrl = authenticationUrl;
     }
 
     /**
-    **/
+     *
+     **/
     public DiscoveryDataDTO challenge(UUID challenge) {
 
         this.challenge = challenge;
         return this;
     }
-    
+
     @ApiModelProperty(value = "")
     @JsonProperty("challenge")
     @Valid
     public UUID getChallenge() {
         return challenge;
     }
+
     public void setChallenge(UUID challenge) {
         this.challenge = challenge;
     }
-
 
 
     @Override
@@ -160,16 +191,17 @@ public class DiscoveryDataDTO  {
         }
         DiscoveryDataDTO discoveryDataDTO = (DiscoveryDataDTO) o;
         return Objects.equals(this.username, discoveryDataDTO.username) &&
-            Objects.equals(this.tennantDomain, discoveryDataDTO.tennantDomain) &&
-            Objects.equals(this.userStoreDomain, discoveryDataDTO.userStoreDomain) &&
-            Objects.equals(this.registrationUrl, discoveryDataDTO.registrationUrl) &&
-            Objects.equals(this.authenticationUrl, discoveryDataDTO.authenticationUrl) &&
-            Objects.equals(this.challenge, discoveryDataDTO.challenge);
+                Objects.equals(this.tennantDomain, discoveryDataDTO.tennantDomain) &&
+                Objects.equals(this.userStoreDomain, discoveryDataDTO.userStoreDomain) &&
+                Objects.equals(this.registrationUrl, discoveryDataDTO.registrationUrl) &&
+                Objects.equals(this.authenticationUrl, discoveryDataDTO.authenticationUrl) &&
+                Objects.equals(this.challenge, discoveryDataDTO.challenge) &&
+                Objects.equals(this.id, discoveryDataDTO.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(username, tennantDomain, userStoreDomain, registrationUrl, authenticationUrl, challenge);
+        return Objects.hash(id, username, tennantDomain, userStoreDomain, registrationUrl, authenticationUrl, challenge);
     }
 
     @Override
@@ -177,7 +209,8 @@ public class DiscoveryDataDTO  {
 
         StringBuilder sb = new StringBuilder();
         sb.append("class DiscoveryDataDTO {\n");
-        
+
+        sb.append("    id: ").append(toIndentedString(id)).append("\n");
         sb.append("    username: ").append(toIndentedString(username)).append("\n");
         sb.append("    tennantDomain: ").append(toIndentedString(tennantDomain)).append("\n");
         sb.append("    userStoreDomain: ").append(toIndentedString(userStoreDomain)).append("\n");
@@ -189,9 +222,9 @@ public class DiscoveryDataDTO  {
     }
 
     /**
-    * Convert the given object to string with each line indented by 4 spaces
-    * (except the first line).
-    */
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
     private String toIndentedString(java.lang.Object o) {
 
         if (o == null) {
--- components/org.wso2.carbon.identity.api.user.biometric.device/org.wso2.carbon.identity.api.user.biometric.device.handler.v1/src/gen/java/org/wso2/carbon/identity/api/user/biometric/device/handler/v1/model/RegistrationRequestDTO.java
+++ components/org.wso2.carbon.identity.api.user.biometric.device/org.wso2.carbon.identity.api.user.biometric.device.handler.v1/src/gen/java/org/wso2/carbon/identity/api/user/biometric/device/handler/v1/model/RegistrationRequestDTO.java
@@ -21,12 +21,15 @@ package org.wso2.carbon.identity.api.user.biometric.device.handler.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
-import java.util.Objects;
+
 import javax.validation.Valid;
+import java.util.Objects;
+
 /**
+ *
  **/
-public class RegistrationRequestDTO  {
-  
+public class RegistrationRequestDTO {
+    private String id;
     private String name;
     private String model;
     private String pushId;
@@ -34,95 +37,125 @@ public class RegistrationRequestDTO  {
     private String signature;
 
     /**
-    **/
+     *
+     **/
+    public RegistrationRequestDTO id(String id) {
+
+        this.id = id;
+        return this;
+    }
+
+    @ApiModelProperty(example = "000011AA", value = "")
+    @JsonProperty("id")
+    @Valid
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    /**
+     *
+     **/
     public RegistrationRequestDTO name(String name) {
 
         this.name = name;
         return this;
     }
-    
+
     @ApiModelProperty(example = "My Device", value = "")
     @JsonProperty("name")
     @Valid
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-    **/
+     *
+     **/
     public RegistrationRequestDTO model(String model) {
 
         this.model = model;
         return this;
     }
-    
+
     @ApiModelProperty(example = "iphone 8", value = "")
     @JsonProperty("model")
     @Valid
     public String getModel() {
         return model;
     }
+
     public void setModel(String model) {
         this.model = model;
     }
 
     /**
-    **/
+     *
+     **/
     public RegistrationRequestDTO pushId(String pushId) {
 
         this.pushId = pushId;
         return this;
     }
-    
+
     @ApiModelProperty(example = "arsresdxvfy556565876", value = "")
     @JsonProperty("pushId")
     @Valid
     public String getPushId() {
         return pushId;
     }
+
     public void setPushId(String pushId) {
         this.pushId = pushId;
     }
 
     /**
-    **/
+     *
+     **/
     public RegistrationRequestDTO publickey(String publickey) {
 
         this.publickey = publickey;
         return this;
     }
-    
+
     @ApiModelProperty(example = "bhkbvhbhjbh756576gfhvbe", value = "")
     @JsonProperty("publickey")
     @Valid
     public String getPublickey() {
         return publickey;
     }
+
     public void setPublickey(String publickey) {
         this.publickey = publickey;
     }
 
     /**
-    **/
+     *
+     **/
     public RegistrationRequestDTO signature(String signature) {
 
         this.signature = signature;
         return this;
     }
-    
+
     @ApiModelProperty(example = "hfcfh576851", value = "")
     @JsonProperty("signature")
     @Valid
     public String getSignature() {
         return signature;
     }
+
     public void setSignature(String signature) {
         this.signature = signature;
     }
-
 
 
     @Override
@@ -136,15 +169,16 @@ public class RegistrationRequestDTO  {
         }
         RegistrationRequestDTO registrationRequestDTO = (RegistrationRequestDTO) o;
         return Objects.equals(this.name, registrationRequestDTO.name) &&
-            Objects.equals(this.model, registrationRequestDTO.model) &&
-            Objects.equals(this.pushId, registrationRequestDTO.pushId) &&
-            Objects.equals(this.publickey, registrationRequestDTO.publickey) &&
-            Objects.equals(this.signature, registrationRequestDTO.signature);
+                Objects.equals(this.model, registrationRequestDTO.model) &&
+                Objects.equals(this.pushId, registrationRequestDTO.pushId) &&
+                Objects.equals(this.publickey, registrationRequestDTO.publickey) &&
+                Objects.equals(this.signature, registrationRequestDTO.signature) &&
+                Objects.equals(this.id, registrationRequestDTO.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, model, pushId, publickey, signature);
+        return Objects.hash(id, name, model, pushId, publickey, signature);
     }
 
     @Override
@@ -152,7 +186,8 @@ public class RegistrationRequestDTO  {
 
         StringBuilder sb = new StringBuilder();
         sb.append("class RegistrationRequestDTO {\n");
-        
+
+        sb.append("    id: ").append(toIndentedString(id)).append("\n");
         sb.append("    name: ").append(toIndentedString(name)).append("\n");
         sb.append("    model: ").append(toIndentedString(model)).append("\n");
         sb.append("    pushId: ").append(toIndentedString(pushId)).append("\n");
@@ -163,9 +198,9 @@ public class RegistrationRequestDTO  {
     }
 
     /**
-    * Convert the given object to string with each line indented by 4 spaces
-    * (except the first line).
-    */
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
     private String toIndentedString(java.lang.Object o) {
 
         if (o == null) {
