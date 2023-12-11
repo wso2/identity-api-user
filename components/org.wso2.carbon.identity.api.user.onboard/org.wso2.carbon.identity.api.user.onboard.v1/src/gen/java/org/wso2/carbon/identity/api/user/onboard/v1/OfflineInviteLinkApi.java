19005b6b072e38de69660af89694c6f92bf557c7
JeethJJ <jihanjeed49@gmail.com>
2023-12-11 11:57:12 +0530

    Add offline-invite-link generation api support.

---
+++ components/org.wso2.carbon.identity.api.user.onboard/org.wso2.carbon.identity.api.user.onboard.v1/src/gen/java/org/wso2/carbon/identity/api/user/onboard/v1/OfflineInviteLinkApi.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.api.user.onboard.v1;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.wso2.carbon.identity.api.user.onboard.v1.model.Error;
+import org.wso2.carbon.identity.api.user.onboard.v1.model.InvitationRequest;
+
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * This API is used to generate a random link that can be used to set a new password.
+ */
+@Path("/offline-invite-link")
+@Api(description = "The offline-invite-link API")
+
+public class OfflineInviteLinkApi  {
+
+    @Autowired
+    private OfflineInviteLinkApiService delegate;
+
+    @Valid
+    @POST
+    
+    @Consumes({ "application/json" })
+    @Produces({ "text/plain", "application/json" })
+    @ApiOperation(value = "Generates a random link that can be used to set a new password", notes = "Generates a " +
+            "random and secured one time link that can be used to set a new password for specified user",
+            response = String.class, authorizations = {
+        @Authorization(value = "BasicAuth"),
+        @Authorization(value = "OAuth2", scopes = {
+            
+        })
+    }, tags = { "Temporary Link" })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 201, message = "Created", response = String.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = Error.class),
+        @ApiResponse(code = 401, message = "Unauthorized", response = Void.class),
+        @ApiResponse(code = 403, message = "Forbidden", response = Void.class),
+        @ApiResponse(code = 500, message = "Server Error", response = Error.class)
+    })
+    public Response generateLink(@ApiParam(value = "This represents the invitation request." , required = true)
+                                     @Valid InvitationRequest invitationRequest) {
+
+        return delegate.generateLink (invitationRequest);
+    }
+
+}
---
+++ components/org.wso2.carbon.identity.api.user.onboard/org.wso2.carbon.identity.api.user.onboard.v1/src/main/java/org/wso2/carbon/identity/api/user/onboard/v1/impl/OfflineInviteLinkApiServiceImpl.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.api.user.onboard.v1.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.wso2.carbon.identity.api.user.onboard.v1.OfflineInviteLinkApiService;
+import org.wso2.carbon.identity.api.user.onboard.v1.model.InvitationRequest;
+import org.wso2.carbon.identity.api.user.onboard.v1.service.OfflineInviteLinkService;
+
+import java.net.URI;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * This API is used to generate a random link that can be used to set a new password.
+ */
+public class OfflineInviteLinkApiServiceImpl implements OfflineInviteLinkApiService {
+
+    private static final Log LOG = LogFactory.getLog(OfflineInviteLinkApiServiceImpl.class);
+    @Autowired
+    private OfflineInviteLinkService offlineInviteLinkService;
+
+    /**
+     * Creates UsersApiServiceImpl.
+     */
+    public OfflineInviteLinkApiServiceImpl() {
+        super();
+    }
+
+    @Override
+    public Response generateLink(InvitationRequest invitationRequest) {
+
+        String generatedLink = offlineInviteLinkService.generatePasswordURL(invitationRequest);
+        return Response.created(URI.create(generatedLink)).entity(generatedLink).build();
+    }
+}
