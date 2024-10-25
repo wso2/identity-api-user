d869d46ddc9373e1a17d75b3613869c12e4201e8
NipuniBhagya <nipunib@wso2.com>
2024-10-25 15:48:03 +0530

    Remove Spring dependencies in user backupcodes API

--- components/org.wso2.carbon.identity.api.user.backupcode/org.wso2.carbon.identity.api.user.backupcode.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/backupcode/v1/MeApi.java
+++ components/org.wso2.carbon.identity.api.user.backupcode/org.wso2.carbon.identity.api.user.backupcode.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/backupcode/v1/MeApi.java
@@ -1,24 +1,26 @@
 /*
- * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2022-2024, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.rest.api.user.backupcode.v1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.wso2.carbon.identity.rest.api.user.backupcode.v1.dto.BackupCodeResponseDTO;
 import org.wso2.carbon.identity.rest.api.user.backupcode.v1.dto.RemainingBackupCodeResponseDTO;
+import org.wso2.carbon.identity.rest.api.user.backupcode.v1.factories.MeApiServiceFactory;
 
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
@@ -35,8 +37,12 @@ import javax.ws.rs.core.Response;
 @io.swagger.annotations.Api(value = "/me", description = "the me API")
 public class MeApi  {
 
-    @Autowired
-    private MeApiService delegate;
+    private final MeApiService delegate;
+
+    public MeApi() {
+
+        this.delegate = MeApiServiceFactory.getMeApi();
+    }
 
     @Valid
     @DELETE
