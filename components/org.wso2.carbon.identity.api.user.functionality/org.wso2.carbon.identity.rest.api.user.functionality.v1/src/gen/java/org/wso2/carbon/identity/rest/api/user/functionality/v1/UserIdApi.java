f5aa8f265752865782bdfc7d26b036e55dfd1ed8
NipuniBhagya <nipunib@wso2.com>
2024-10-25 20:24:25 +0530

    Remove Spring dependencies in user functionality API

--- components/org.wso2.carbon.identity.api.user.functionality/org.wso2.carbon.identity.rest.api.user.functionality.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/functionality/v1/UserIdApi.java
+++ components/org.wso2.carbon.identity.api.user.functionality/org.wso2.carbon.identity.rest.api.user.functionality.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/functionality/v1/UserIdApi.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020-2024, WSO2 LLC. (http://www.wso2.com).
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,8 +18,7 @@
 
 package org.wso2.carbon.identity.rest.api.user.functionality.v1;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import org.wso2.carbon.identity.rest.api.user.functionality.v1.factories.UserIdApiServiceFactory;
 import org.wso2.carbon.identity.rest.api.user.functionality.v1.model.Error;
 import org.wso2.carbon.identity.rest.api.user.functionality.v1.model.LockStatusResponse;
 import org.wso2.carbon.identity.rest.api.user.functionality.v1.model.StatusChangeRequest;
@@ -34,8 +33,12 @@ import io.swagger.annotations.*;
 
 public class UserIdApi {
 
-    @Autowired
-    private UserIdApiService delegate;
+    private final UserIdApiService delegate;
+
+    public UserIdApi() {
+
+        this.delegate = UserIdApiServiceFactory.getUserIdApi();
+    }
 
     @Valid
     @PUT
