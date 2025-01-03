ee8f3f1feeee733ebc75d6c5de04e08c9b2d1bff
Nipuni Paaris <nipunib@wso2.com>
2025-01-03 09:31:16 +0530

    [Spring Cleanup] Remove Spring dependencies in user functionality API (#218)
    * Upgrade repo versions
    
    * [WSO2 Release] [Jenkins #348] [Release 1.3.44] prepare release v1.3.44
    
    * [WSO2 Release] [Jenkins #348] [Release 1.3.44] prepare for next development iteration
    
    * Remove Spring dependencies in user functionality API
    
    * Revert "Upgrade repo versions"
    
    This reverts commit 5e8cd472c0cc68473be7118c1d4460029108fea7.
    
    * Revert version bump
    
    ---------
    
    Co-authored-by: asha15 <165079T@uom.lk>
    Co-authored-by: WSO2 Builder <jenkins-bot@wso2.com>

--- components/org.wso2.carbon.identity.api.user.functionality/org.wso2.carbon.identity.rest.api.user.functionality.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/functionality/v1/MeApi.java
+++ components/org.wso2.carbon.identity.api.user.functionality/org.wso2.carbon.identity.rest.api.user.functionality.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/functionality/v1/MeApi.java
@@ -1,31 +1,32 @@
 /*
-* Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2020-2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.wso2.carbon.identity.rest.api.user.functionality.v1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 import java.io.InputStream;
 import java.util.List;
 
+import org.wso2.carbon.identity.rest.api.user.functionality.v1.factories.MeApiServiceFactory;
 import org.wso2.carbon.identity.rest.api.user.functionality.v1.model.Error;
 import org.wso2.carbon.identity.rest.api.user.functionality.v1.model.LockStatusResponse;
 import org.wso2.carbon.identity.rest.api.user.functionality.v1.model.UserStatusChangeRequest;
-import org.wso2.carbon.identity.rest.api.user.functionality.v1.MeApiService;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -39,8 +40,12 @@ import javax.validation.constraints.*;
 
 public class MeApi  {
 
-    @Autowired
-    private MeApiService delegate;
+    private final MeApiService delegate;
+
+    public MeApi() {
+
+        this.delegate = MeApiServiceFactory.getMeApi();
+    }
 
     @Valid
     @PUT
