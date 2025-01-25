69524075c5c3c524e02c99c534442ee9370ef9b0
lashini <lashini@wso2.com>
2025-01-25 21:13:46 +0530

    [Spring Cleanup] Remove Spring dependencies in user TOTP API

--- components/org.wso2.carbon.identity.api.user.totp/org.wso2.carbon.identity.api.user.totp.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/totp/v1/MeApi.java
+++ components/org.wso2.carbon.identity.api.user.totp/org.wso2.carbon.identity.api.user.totp.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/totp/v1/MeApi.java
@@ -1,22 +1,23 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.rest.api.user.totp.v1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.wso2.carbon.identity.rest.api.user.totp.v1.dto.*;
 import org.wso2.carbon.identity.rest.api.user.totp.v1.MeApiService;
 import org.wso2.carbon.identity.rest.api.user.totp.v1.factories.MeApiServiceFactory;
@@ -44,8 +45,12 @@ import javax.ws.rs.*;
 @io.swagger.annotations.Api(value = "/me", description = "the me API")
 public class MeApi  {
 
-   @Autowired
-   private MeApiService delegate;
+    private final MeApiService delegate;
+
+    public MeApi() {
+
+        this.delegate = MeApiServiceFactory.getMeApi();
+    }
 
     @Valid
     @DELETE
--- components/org.wso2.carbon.identity.api.user.totp/org.wso2.carbon.identity.api.user.totp.v1/src/main/java/org/wso2/carbon/identity/rest/api/user/totp/v1/impl/MeApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.user.totp/org.wso2.carbon.identity.api.user.totp.v1/src/main/java/org/wso2/carbon/identity/rest/api/user/totp/v1/impl/MeApiServiceImpl.java
@@ -1,24 +1,27 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.wso2.carbon.identity.rest.api.user.totp.v1.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.wso2.carbon.identity.rest.api.user.totp.v1.MeApiService;
 import org.wso2.carbon.identity.rest.api.user.totp.v1.core.TOTPService;
 import org.wso2.carbon.identity.rest.api.user.totp.v1.dto.UserRequestDTO;
+import org.wso2.carbon.identity.rest.api.user.totp.v1.factories.TOTPServiceFactory;
 
 import javax.ws.rs.core.Response;
 
@@ -31,8 +34,12 @@ import static org.wso2.carbon.identity.api.user.totp.common.TOTPConstants.ErrorM
  */
 public class MeApiServiceImpl extends MeApiService {
 
-    @Autowired
-    TOTPService totpService;
+    private final TOTPService totpService;
+
+    public MeApiServiceImpl() {
+
+        this.totpService = TOTPServiceFactory.getTOTPService();
+    }
 
     @Override
     public Response meTotpDelete() {
