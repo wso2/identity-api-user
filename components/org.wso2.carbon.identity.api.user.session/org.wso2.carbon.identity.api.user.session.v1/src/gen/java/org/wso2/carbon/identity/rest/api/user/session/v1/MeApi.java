fa748bc9ffcf9789cb001bed2250f24cf7098c4f
lashini <lashini@wso2.com>
2025-01-25 15:11:34 +0530

    [Spring Cleanup] Remove Spring dependencies in the Session Management API

--- components/org.wso2.carbon.identity.api.user.session/org.wso2.carbon.identity.api.user.session.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/session/v1/MeApi.java
+++ components/org.wso2.carbon.identity.api.user.session/org.wso2.carbon.identity.api.user.session.v1/src/gen/java/org/wso2/carbon/identity/rest/api/user/session/v1/MeApi.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.rest.api.user.session.v1;
@@ -21,8 +23,8 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.wso2.carbon.identity.rest.api.user.session.v1.dto.SessionsDTO;
+import org.wso2.carbon.identity.rest.api.user.session.v1.factories.MeApiServiceFactory;
 
 import javax.validation.Valid;
 import javax.ws.rs.DELETE;
@@ -37,8 +39,12 @@ import javax.ws.rs.core.Response;
 @Api(value = "/me", description = "the me API")
 public class MeApi {
 
-    @Autowired
-    private MeApiService delegate;
+    private final MeApiService delegate;
+
+    public MeApi () {
+
+        this.delegate = MeApiServiceFactory.getMeApi();
+    }
 
     @Valid
     @GET
