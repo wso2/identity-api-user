021098c9d1906eaa2b306fc02653ca68575e3d68
lashini <lashini@wso2.com>
2025-01-25 15:32:23 +0530

    [Spring Cleanup] Remove Spring dependencies in user association API

--- components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/main/java/org/wso2/carbon/identity/rest/api/user/association/v1/core/UserAssociationService.java
+++ components/org.wso2.carbon.identity.api.user.association/org.wso2.carbon.identity.api.user.association.v1/src/main/java/org/wso2/carbon/identity/rest/api/user/association/v1/core/UserAssociationService.java
@@ -1,18 +1,21 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.wso2.carbon.identity.rest.api.user.association.v1.core;
 
 import org.apache.commons.lang.StringUtils;
@@ -27,18 +30,21 @@ import org.wso2.carbon.identity.rest.api.user.association.v1.dto.FederatedAssoci
 import org.wso2.carbon.identity.rest.api.user.association.v1.dto.FederatedAssociationRequestDTO;
 import org.wso2.carbon.identity.rest.api.user.association.v1.dto.IdpDTO;
 import org.wso2.carbon.identity.rest.api.user.association.v1.dto.UserDTO;
-import org.wso2.carbon.identity.rest.api.user.association.v1.util.UserAssociationServiceHolder;
+import org.wso2.carbon.identity.user.account.association.UserAccountConnector;
 import org.wso2.carbon.identity.user.account.association.dto.UserAccountAssociationDTO;
 import org.wso2.carbon.identity.user.account.association.exception.UserAccountAssociationClientException;
 import org.wso2.carbon.identity.user.account.association.exception.UserAccountAssociationException;
+import org.wso2.carbon.identity.user.profile.mgt.association.federation.FederatedAssociationManager;
 import org.wso2.carbon.identity.user.profile.mgt.association.federation.exception.FederatedAssociationManagerClientException;
 import org.wso2.carbon.identity.user.profile.mgt.association.federation.exception.FederatedAssociationManagerException;
 import org.wso2.carbon.identity.user.profile.mgt.association.federation.model.FederatedAssociation;
+import org.wso2.carbon.user.core.service.RealmService;
 import org.wso2.carbon.user.core.util.UserCoreUtil;
 import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.ws.rs.core.Response;
 
 import static org.wso2.carbon.identity.api.user.common.Constants.ERROR_CODE_DELIMITER;
@@ -53,11 +59,24 @@ public class UserAssociationService {
 
     private static final Log log = LogFactory.getLog(UserAssociationService.class);
 
+    private final UserAccountConnector userAccountConnector;
+    private final FederatedAssociationManager federatedAssociationManager;
+    private final RealmService realmService;
+
+    public UserAssociationService(UserAccountConnector userAccountConnector, FederatedAssociationManager
+            federatedAssociationManager, RealmService realmService) {
+
+        this.userAccountConnector = userAccountConnector;
+        this.federatedAssociationManager = federatedAssociationManager;
+        this.realmService = realmService;
+    }
+
     public List<UserDTO> getAssociationsOfUser(String userId) {
 
         try {
-            UserAccountAssociationDTO[] accountAssociationsOfUser = UserAssociationServiceHolder
-                    .getUserAccountConnector().getAccountAssociationsOfUser(userId);
+            UserAccountAssociationDTO[] accountAssociationsOfUser = userAccountConnector
+                    .getAccountAssociationsOfUser(userId);
+
             return getUserAssociationsDTOs(accountAssociationsOfUser);
         } catch (UserAccountAssociationException e) {
             throw handleUserAccountAssociationException(e, "Error while getting associations of user: " + userId);
@@ -67,8 +86,8 @@ public class UserAssociationService {
     public List<FederatedAssociationDTO> getFederatedAssociationsOfUser(String userId) {
 
         try {
-            FederatedAssociation[] federatedAccountAssociationsOfUser = UserAssociationServiceHolder
-                    .getFederatedAssociationManager().getFederatedAssociationsOfUser(getUser(userId));
+            FederatedAssociation[] federatedAccountAssociationsOfUser = federatedAssociationManager
+                    .getFederatedAssociationsOfUser(getUser(userId));
             return getFederatedAssociationDTOs(federatedAccountAssociationsOfUser);
         } catch (FederatedAssociationManagerException e) {
             throw handleFederatedAssociationManagerException(e, "Error while getting associations of user: " + userId);
@@ -82,19 +101,18 @@ public class UserAssociationService {
                 throw new UserAccountAssociationClientException(ERROR_CODE_PW_MANDATORY.getCode(),
                         ERROR_CODE_PW_MANDATORY.getDescription());
             }
-            UserAssociationServiceHolder.getUserAccountConnector()
-                    .createUserAccountAssociation(associationUserRequestDTO.getUserId(),
-                            associationUserRequestDTO.getPassword().toCharArray());
+            userAccountConnector.createUserAccountAssociation(associationUserRequestDTO.getUserId(),
+                    associationUserRequestDTO.getPassword().toCharArray());
         } catch (UserAccountAssociationException e) {
             throw handleUserAccountAssociationException(e,
                     "Error while adding associations of user: " + associationUserRequestDTO.getUserId());
         }
     }
 
-    public boolean switchLoggedInUser(String userName)  {
+    public boolean switchLoggedInUser(String userName) {
 
         try {
-            return UserAssociationServiceHolder.getUserAccountConnector().switchLoggedInUser(userName);
+            return userAccountConnector.switchLoggedInUser(userName);
         } catch (UserAccountAssociationException e) {
             throw handleUserAccountAssociationException(e, "Error while switching user: " + userName);
         }
@@ -103,7 +121,7 @@ public class UserAssociationService {
     public void deleteUserAccountAssociation(String userId) {
 
         try {
-            UserAssociationServiceHolder.getUserAccountConnector().deleteUserAccountAssociation(userId);
+            userAccountConnector.deleteUserAccountAssociation(userId);
         } catch (UserAccountAssociationException e) {
             throw handleUserAccountAssociationException(e, "Error while deleting user association: " + userId);
         }
@@ -112,8 +130,7 @@ public class UserAssociationService {
     public void deleteAssociatedUserAccount(String ownerUserId, String associatedUserId) {
 
         try {
-            UserAssociationServiceHolder.getUserAccountConnector().deleteAssociatedUserAccount(ownerUserId,
-                    associatedUserId);
+            userAccountConnector.deleteAssociatedUserAccount(ownerUserId, associatedUserId);
         } catch (UserAccountAssociationException e) {
             throw handleUserAccountAssociationException(e, "Error while deleting user association of the user: "
                     + ownerUserId + ", with the user: " + associatedUserId);
@@ -123,8 +140,7 @@ public class UserAssociationService {
     public void deleteFederatedUserAccountAssociation(String userId, String federatedAssociationId) {
 
         try {
-            UserAssociationServiceHolder.getFederatedAssociationManager()
-                    .deleteFederatedAssociation(getUser(userId), federatedAssociationId);
+            federatedAssociationManager.deleteFederatedAssociation(getUser(userId), federatedAssociationId);
         } catch (FederatedAssociationManagerException e) {
             throw handleFederatedAssociationManagerException(e, "Error while deleting federated user association: "
                     + userId);
@@ -134,7 +150,7 @@ public class UserAssociationService {
     public void deleteFederatedUserAccountAssociation(String userId) {
 
         try {
-            UserAssociationServiceHolder.getFederatedAssociationManager().deleteFederatedAssociation(getUser(userId));
+            federatedAssociationManager.deleteFederatedAssociation(getUser(userId));
         } catch (FederatedAssociationManagerException e) {
             throw handleFederatedAssociationManagerException(e, "Error while deleting federated user association: "
                     + userId);
@@ -294,7 +310,7 @@ public class UserAssociationService {
         user.setUserName(userAccountAssociationDTO.getUsername());
         user.setUserStoreDomain(userAccountAssociationDTO.getDomain());
         user.setTenantDomain(userAccountAssociationDTO.getTenantDomain());
-        return new UserToUniqueId().apply(UserAssociationServiceHolder.getRealmService(), user);
+        return new UserToUniqueId().apply(realmService, user);
     }
 
     private User getUser(String userId) {
