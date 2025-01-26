3253fa2f83a6759ae5d5ff15540b02bb01be067e
Lashini Jayasekara <30428591+lashinijay@users.noreply.github.com>
2025-01-26 07:49:14 +0530

    [Spring Cleanup] Remove Spring dependencies in user MFA API (#251)

---
+++ components/org.wso2.carbon.identity.api.user.mfa/org.wso2.carbon.identity.api.user.mfa.v1/src/main/java/org/wso2/carbon/identity/rest/api/user/mfa/v1/factories/MFAServiceFactory.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.rest.api.user.mfa.v1.factories;
+
+import org.wso2.carbon.identity.rest.api.user.mfa.v1.core.MFAService;
+import org.wso2.carbon.identity.rest.api.user.mfa.v1.util.UserMFAServiceHolder;
+import org.wso2.carbon.user.core.service.RealmService;
+
+/**
+ * Factory class for MFAService.
+ */
+public class MFAServiceFactory {
+
+    private MFAServiceFactory() {
+
+    }
+
+    private static class MFAServicesHolder {
+
+        private static final MFAService SERVICE = createServiceInstance();
+    }
+
+    private static MFAService createServiceInstance() {
+
+        RealmService realmService = getRealmService();
+        return new MFAService(realmService);
+    }
+
+    /**
+     * Get MFAService service.
+     *
+     * @return MFAService.
+     */
+    public static MFAService getMFAService() {
+
+        return MFAServicesHolder.SERVICE;
+    }
+
+    private static RealmService getRealmService() {
+
+        RealmService service = UserMFAServiceHolder.getRealmService();
+        if (service == null) {
+            throw new IllegalStateException("RealmService is not available from OSGi context.");
+        }
+        return service;
+    }
+}
--- components/org.wso2.carbon.identity.api.user.mfa/org.wso2.carbon.identity.api.user.mfa.v1/src/main/java/org/wso2/carbon/identity/rest/api/user/mfa/v1/impl/MeApiServiceImpl.java
+++ components/org.wso2.carbon.identity.api.user.mfa/org.wso2.carbon.identity.api.user.mfa.v1/src/main/java/org/wso2/carbon/identity/rest/api/user/mfa/v1/impl/MeApiServiceImpl.java
@@ -1,27 +1,28 @@
 /*
- * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com) All Rights Reserved.
+ * Copyright (c) 2022-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.rest.api.user.mfa.v1.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.wso2.carbon.identity.rest.api.user.mfa.v1.MeApiService;
 import org.wso2.carbon.identity.rest.api.user.mfa.v1.core.MFAService;
 
 import org.wso2.carbon.identity.rest.api.user.mfa.v1.dto.EnabledAuthenticatorsDTO;
-
+import org.wso2.carbon.identity.rest.api.user.mfa.v1.factories.MFAServiceFactory;
 
 import javax.ws.rs.core.Response;
 
@@ -30,8 +31,16 @@ import javax.ws.rs.core.Response;
  */
 public class MeApiServiceImpl extends MeApiService {
 
-    @Autowired
-    MFAService mfaService;
+    private final MFAService mfaService;
+
+    public MeApiServiceImpl() {
+
+        try {
+            this.mfaService = MFAServiceFactory.getMFAService();
+        } catch (IllegalStateException e) {
+            throw new RuntimeException("Error occurred while initiating the MFAService.", e);
+        }
+    }
 
     @Override
     public Response meMfaAuthenticatorsGet() {
